d2bdbf08f3477122cbd8fa9a116ae423a1a7a4dd
aijaza1 <59926329+aijaza1@users.noreply.github.com>
2020-01-15 10:25:32 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*****************************
+*Hello
+******************************
+*Azeem Aijaz
+*1/15/2020
+*CMSC-255-003
+******************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("<Azeem Aijaz>");
+		System.out.println("<Hello>");
+		System.out.println("<intro to course>");
+		System.out.println("<1/15/2020>");
+		System.out.println("<CMSC-255-003>");
+		System.out.println();
+	
+	}
+
+}
